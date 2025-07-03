65a92d198eff847feb9eceebee2fe00aeb580eb4
phich82 <phich82@gmail.com>
2025-07-03 17:54:32 +0700

    python,array

---
+++ tutorial/21-python_array.py
@@ -0,0 +1,20 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+print(cars)
+# array is a special varible,can hold more than one value at a time
+# python don't do array but list could us as subtitude
+
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+x = cars[0]
+print(f'list of car print out first item from this list after using x = cars[] {cars[0]} ')
+#
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+cars[0] = "Toyota"
+print(f'list of car after replace first item from this list after using x = cars[] = item {cars}')
+
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
+
---
+++ tutorial/21-python_array1.py
@@ -0,0 +1,18 @@
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+  print(x)
+# for-in use to list out all the item in the list mean print out all the element/item in this list
+
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+cars.append("Honda")
+print(f'array cars add item after append() {cars}')
+# 'append' use for adding item in array
+
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+cars.pop(1)
+print(f'array cars remove  item after pop() {cars}')
+
+
+
